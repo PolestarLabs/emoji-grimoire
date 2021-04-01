0058265239b1fe71730d6de6caa2d09eb16b4d19
Tsumari <lucasflicky@hotmail.com>
2021-04-01 14:37:19 -0300

    bruh

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const  { PolluxEmoji, createShallowEmojiBank } = require("./PolluxEmojiClass");
 module.exports = {
     initialize(PLX){
         createShallowEmojiBank(PLX);
-        global._emoji(e,f) = new PolluxEmoji(e,f);
+        global._emoji = (e,f) => new PolluxEmoji(e,f);
     }
 }
